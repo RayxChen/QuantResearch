0b1f37cac6ff1e49e924242c364f4835be40f0a3
rayxchen <144602374+RayxChen@users.noreply.github.com>
2023-09-17 20:23:32 +0100

    Add files via upload

---
+++ HighFrequency/OrderBookTime.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+#import WebSocket client library (and others)
+import sys
+import json
+import signal
+import time
+import _thread
+import websocket
+
+import pandas as pd
+from itertools import chain
+
+
+# Parse command line arguments (symbol and depth)
+if len(sys.argv) < 3:
+    sys.exit(1)
+else:
+    api_symbol = sys.argv[1]
+    api_depth = int(sys.argv[2])
+
+
+# Define order book variables
+api_book = []
+
+def to_int(x, shift):
+    return int(float(x)*shift)
+
+
+#price_pow = 100, vol_pow = 10**8, timestamp_pow = 10**6
+def build_int(price, vol, ts):
+    '''price, vol, ts: string,
+    timestamp string: '1665671311.165199', rm starting 16'''
+    inte, frac = ts.split('.')
+    return to_int(price+'0', 100), to_int(vol, 100000000), int(inte[2:]), int(frac)
+
+
+
+def api_book_update(api_book_side, api_book_data):
+        #for data in api_book_data:
+        #price_level = data[0], volume = data[1], timestamp = data[2]
+
+    side = [1 if api_book_side=='ask' else 0 for _ in range(len(api_book_data))]
+    # 4th col is 'r' -> flag of republish
+    data = [build_int(x[0], x[1], x[2]) for x in api_book_data]
+
+    api_book.append([[x[0]+y, *x[1:]] for x, y in zip(data, side)])
+
+# Define WebSocket callback functions
+def ws_thread(*args):
+    ws = websocket.WebSocketApp('wss://ws.kraken.com/', on_open=ws_open, on_message=ws_message)
+    ws.run_forever()
+
+def ws_open(ws):
+    ws.send('{"event":"subscribe", "subscription":{"name":"book", "depth":%(api_depth)d}, "pair":["%(api_symbol)s"]}' % {'api_depth':api_depth, 'api_symbol':api_symbol})
+
+def ws_message(ws, ws_data):
+    api_data = json.loads(ws_data)
+#    print('api_data',api_data)
+    if 'event' in api_data:
+        return
+    else:
+        # initial snapshot
+        if 'as' in api_data[1]:
+            api_book_update('ask', api_data[1]['as'])
+            api_book_update('bid', api_data[1]['bs'])
+        # update
+        else:
+            for data in api_data[1:len(api_data)-2]:
+                if 'a' in data:
+                    api_book_update('ask', data['a'])
+                elif 'b' in data:
+                    api_book_update('bid', data['b'])
+
+def save_data(data):
+    df = pd.DataFrame(data, columns=['price_side', 'volume', 'time_inte', 'time_frac'], dtype='uint32')
+    df.to_hdf('new_data.h5',key='kraken_OB_time', mode='a')
+
+
+if __name__ == "__main__":
+    
+    # Start new thread for WebSocket interface
+    _thread.start_new_thread(ws_thread, ())
+    
+    # Output order book (once per second) in main thread
+    try:
+        while True:
+            api_book = list(chain(*api_book))
+            api_book.sort(key=lambda x:(x[2], x[3]))
+
+            print(api_book)
+            save_data(api_book)
+            api_book.clear()
+            time.sleep(1)
+        
+    except KeyboardInterrupt:
+        sys.exit(0)
